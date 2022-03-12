db9ba0f9688ad16341cff8365b0eb49e8f4b850c
Ruben A. Ramirez <51794934+RubesWorld@users.noreply.github.com>
2022-03-11 23:27:32 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,33 +2,21 @@ import { useState } from "react";
 import "./styles/app.scss";
 import data from "./data";
 
+import Header from "./components/Header";
+import Searchbar from "./components/Searchbar";
+import TeamTabs from "./components/TeamTabs";
+
 export default function App() {
   const [teams, setteams] = useState(data);
   return (
     <div className="App">
-      <h1>California Sports Teams </h1>
-      <h2>
-        California has the most sports teams from any state. See your favorite
-        below!
-      </h2>
-
-      <label htmlFor="search">Search your favorite team: </label>
-      <input type="text" id="search" />
+      <Header />
+      <Searchbar />
 
       <div className="container">
-        <div className="team-tab">
-          {data.map((team, key) => (
-            <div index={key}>
-              <h1>{team.name}</h1>
-              <h2>
-                {team.city}, {team.state}
-              </h2>
-              <span>{team.sport}</span>
-              <br />
-              <button>Favorite</button>
-            </div>
-          ))}
-        </div>
+        {data.map((team, key) => (
+          <TeamTabs team={team} index={key} />
+        ))}
       </div>
     </div>
   );
