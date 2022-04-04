a54cbafa282d4ec6e7fe00322c37d13d4b284c33
Ruben A. Ramirez <51794934+RubesWorld@users.noreply.github.com>
2022-04-03 21:49:32 -0400

    initial commit

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,7 +1,10 @@
 const Searchbar = ({ onSearch, onChange, value, id }) => {
   return (
     <div>
-      <label htmlFor="search">Search your favorite teams:</label>
+      <label htmlFor="search">
+        Select your favorite teams and see what your fandom's success has looked
+        like:{" "}
+      </label>
       <input type="text" id={id} onChange={onChange} value={value} />
       <br />
       <br />
